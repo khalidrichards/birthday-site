d63ec4710a6a74c9966f5afd5fcb083f2b7ce565
khalidrichards <119441511+khalidrichards@users.noreply.github.com>
2023-06-06 21:55:28 -0400

    Added bootstrap; did some very bare bones styling. Next is styling the events section.

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,6 @@
 function getDaysUntilBirthday() {
     var birthday = new Date("09/11/2023");
     var today = new Date();
-
     var timeDifference = birthday.getTime() - today.getTime();
     var timeDifferenceInDays = timeDifference / (1000 * 3600 * 24);
     console.log("It is " + timeDifferenceInDays + " days until your birthday.");
@@ -10,7 +9,9 @@ function getDaysUntilBirthday() {
 
 function updateCountdown() {
     var daysUntilBirthday = getDaysUntilBirthday();
-    var countdownMessage = "It is <b>" + daysUntilBirthday + "</b> days until Khalid's birthday!";
+    var verbForDays = daysUntilBirthday === 1 ? 'is' : 'are';
+    var wordForDays = daysUntilBirthday === 1 ? 'day' : 'days';
+    var countdownMessage = "There " + verbForDays + "<font size=24><b>" + daysUntilBirthday + "</b></font> " + wordForDays + " until Khalid's birthday!";
     var countdown = document.getElementById('countdown');
     countdown.innerHTML = "<p>" + countdownMessage + "</p>";
 }
--- js/events.js
+++ js/events.js
@@ -1,5 +1,6 @@
 var BR_TAG = "<br />"
-
+var FORMS_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLScO5VYHAXun4djnJ39Buj9hBUGknxBlK2BGbAfpJRJNAcs0UA/viewform?usp=pp_url&entry.915385680=Your+Name+Here';
+var events_text = 'Below is a listing of the events planned for the weekend! Please take a look at fill out <a href="' + FORMS_LINK + '">this form</a> to let me know what you\'d like to attend.';
 function wrapInH2(text) {
     return "<h2>" + text + "</h2>";
 }
@@ -11,19 +12,24 @@ function wrapInI(text) {
 }
 
 function renderEvent(eventInfo) {
-    var eventHtml = '<div class="event_info">';
+    var eventHtml = '<div class="event_info col">';
     eventHtml += wrapInH2(eventInfo.event_title) + BR_TAG;
     eventHtml += wrapInP(eventInfo.date + " --- " + eventInfo.start_time) + BR_TAG;
     eventHtml += wrapInP(eventInfo.event_location) + BR_TAG;
     eventHtml += wrapInP(wrapInI(eventInfo.event_description)) + BR_TAG + BR_TAG;
+    eventHtml += '</div>';
     return eventHtml;
 }
 
 function renderAllEvents(events) {
     eventsInnerHtml = '<h1> Events </h1>';
+    eventsInnerHtml += BR_TAG + wrapInP(events_text);
     for(var i = 0; i < events.length; i++) {
+        var makeNewRow = i % 2 === 0;
         var event = events[i];
+        if (makeNewRow) { eventsInnerHtml += '<div class="row">' }
         eventsInnerHtml += renderEvent(event);
+        if (!makeNewRow) { eventsInnerHtml += '</div>'}
     }
     var eventsElement = document.getElementById('events');
     eventsElement.innerHTML += eventsInnerHtml;
